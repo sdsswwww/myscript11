8086b80d787126793cbfe0d928198e1a4b588c91
bigcat <bigcat123123@qq.com>
2025-08-09 21:05:32 +0800

    1

---
+++ alertdownload.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         alert download
+// @namespace    http://tampermonkey.net/
+// @version      2025-08-09
+// @description  try to take over the world!
+// @author       You
+// @match        https://windfiles.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=windfiles.com
+// @updateURL   https://raw.githubusercontent.com/sdsswwww/myscript11/refs/heads/main/alertdownload.js
+// @grant        none
+// ==/UserScript==
+
+function work() {
+    'use strict';
+
+    // Your code here...
+    // <input type="submit" value="Start Download Now">
+    const playSound = async () => {
+        const audio = new Audio('https://actions.google.com/sounds/v1/alarms/beep_short.ogg');
+        for (let i = 0; i < 10; i++) {
+            await audio.play();
+            await new Promise(resolve => setTimeout(resolve, 100));
+            audio.currentTime = 0;
+        }
+    };
+
+    const input = document.querySelector('input[type="submit"][value="Start Download Now"]');
+    if (input) {
+        playSound();
+    }
+}
+
+const intervalId = setInterval(() => {
+    work();
+}, 5000); // Check every 5 seconds
