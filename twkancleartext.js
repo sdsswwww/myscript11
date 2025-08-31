ea688fcecc99e5e63a9b76f1fb33b6b3c2bf7689
bigcat <bigcat123123@qq.com>
2025-08-31 17:28:55 +0800

    1

--- twkancleartext.js
+++ twkancleartext.js
@@ -2,8 +2,9 @@
 // @name        twkan.com clear text
 // @namespace   Violentmonkey Scripts
 // @match       https://twkan.com/*
+// @match       https://cn.wa01.com/*
 // @grant       none
-// @version     1.6
+// @version     1.7
 // @author      -
 // @description 3/25/2025, 11:29:27 PM
 // @updateURL   https://raw.githubusercontent.com/sdsswwww/myscript11/refs/heads/main/twkancleartext.js
@@ -11,14 +12,16 @@
 
   
 
-let z = [ 'TWKAN',  'ᴛᴡᴋᴀɴ',  '𝗍𝗐𝗄𝖺𝗇', '臺湾小説网', "𝕥𝕨𝕜𝕒𝕟", "最⊥新⊥小⊥说⊥"  ]
-
+let z = [ 'TTKAN', 'TWKAN',  'ᴛᴛᴋᴀɴ',  'ᴛᴡᴋᴀɴ',  '𝗍𝗍𝗄𝖺𝗇', '𝗍𝗐𝗄𝖺𝗇', '臺湾小説网', "𝕥𝕥𝕜𝕒𝕟","𝕥𝕨𝕜𝕒𝕟", "最⊥新⊥小⊥说⊥"  ]
+// normalize to lowercase, trim and remove duplicates
+z = Array.from(new Set(z.map(s =>  s.toLowerCase().trim() ).filter(Boolean)));
+console.log(z);
 function work() {
     console.log("twkancleartext.js is running");
     const contentDiv = document.getElementById("txtcontent");
     if (contentDiv) {
         const lines = contentDiv.innerHTML.split("<br>");
-        const filteredLines = lines.filter(line => !z.some(word => line.includes(word)));
+        const filteredLines = lines.filter(line => !z.some(word => line.toLowerCase().includes(word)));
         contentDiv.innerHTML = filteredLines.join("<br>");
         if (lines.length != filteredLines.length) return true;
     }
